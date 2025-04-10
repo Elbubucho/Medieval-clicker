96decd68ac1bdf2b6ac380d122bd4a4dc95092e8
Elbubucho <valmax.sala@gmail.com>
2025-04-10 11:45:23 +0200

    Ajout de l'arrondi pour pas avoir de decimal a l'ecran de l'utilisateur

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 
 /* Incrementation du nombre d'épée au click */
 let sword = document.querySelector('.sword_nbr');
-let swordNbr = parseFloat(sword.innerHTML);
+let swordNbr = Math.round(parseFloat(sword.innerHTML));
 let smithBtn = document.querySelector('.smith_button');
 
 function incrementSmith() {
@@ -14,33 +14,40 @@ incrementSmith();
 
 /* achat d'un autoclicker */
 
+
+let clickerSpS = 0;
+
+function buyClicker(){
+
 let clickerCost = document.querySelector('.clicker_cost');
 let parsedClickerCost = parseFloat(clickerCost.innerHTML);
-
 let clicker = document.querySelector('.shop_content');
-
 let clickerLevel = document.querySelector('.clicker_lvl');
 let parsedClickerLevel = parseFloat(clickerLevel.innerHTML);
 let clickerIncrease = document.querySelector('.clicker_increase');
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
-let clickerSpS = 0;
 
-function buyClicker(){
     clicker.addEventListener('click', () => {
     if (swordNbr >= parsedClickerCost) {
-       swordNbr = sword.innerHTML -= parsedClickerCost;
+       swordNbr-= parsedClickerCost;
+       swordNbr = Math.floor(swordNbr);
+       sword.innerHTML = swordNbr;
        clickerLevel.innerHTML ++;
        clickerIncrease.innerHTML = ++ parsedClickerIncrease;
-       clickerCost.innerHTML = parsedClickerCost *= 2;
+       clickerCost.innerHTML = Math.round(parsedClickerCost *= 1.5);
        clickerSpS = clickerIncrease.innerHTML - 1;
        }
  });  
- function autoclicker () {
-            swordNbr += clickerSpS;
-        sword.innerHTML = swordNbr;
-        }
-        setInterval(autoclicker,1000);
 }
 
 buyClicker();
+/* lancement de l'autoclicker */
 
+function autoclicker () {
+    swordNbr += clickerSpS;
+    swordNbr = Math.floor(swordNbr);
+sword.innerHTML = swordNbr;
+}
+setInterval(autoclicker,1000);
+
+autoclicker();
