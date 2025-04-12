9e49cb6b8011894bec97214f50ecf1d2ae58566d
Elbubucho <valmax.sala@gmail.com>
2025-04-13 00:53:48 +0200

    amélioration du responsive design et de l'UI, ajoute d'une fonction pour griser les objets lorsqu'on n'as pas assez d'argent

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ let smithBtn = document.querySelector('.smith_button');
 let swordPerClick = 0;
 let wholeSpS = 0;
 
-/* Incrementation du nombre d'épée au click en fonction des objets achetés */
+/* Incrementation du nombre d'épée au click en fonction des objets achetés et MAJ du nbr affiché */
 function incrementSmith() {
     smithBtn.addEventListener('click', () => {
         swordNbr += swordPerClick;
@@ -14,14 +14,16 @@ function incrementSmith() {
 
 };
 incrementSmith();
+/* MAJ de l'affichage generé passivement */
+
+
 
 /* achat des objets */
-
 let shopItems = {
     clicker : {
         cost:10,
         level:0,
-        costMultiplier:1.6,
+        costMultiplier:1.3,
         spsIncrement:1,
         type:'morePerSeconde',
         element: document.querySelector('.shop_content.cursor'),
@@ -56,8 +58,8 @@ let shopItems = {
     oven: {
         cost:7000,
         level:0,
-        costMultiplier:1.6,
-        spsIncrement:15,
+        costMultiplier:2,
+        spsIncrement:50,
         type:'morePerClick',
         element: document.querySelector('.shop_content.oven'),
         costElement:document.querySelector('.oven_cost'),
@@ -68,9 +70,7 @@ let shopItems = {
 };
 
 function buyItems(){
-
     let shopContent = document.querySelectorAll('.shop_content');
-
     shopContent.forEach(shopContent => {
         shopContent.addEventListener('click', () => {
 
@@ -104,6 +104,18 @@ function buyItems(){
                 else{
                     swordPerClick+= shopItems[itemData].spsIncrement;
                 }
+
+                function swordPerSeconde() {
+                    let returnSpS = document.querySelector('.sps_counter');
+                    let parsedReturnSpS = parseFloat(returnSpS.innerHTML);
+                
+                        if (wholeSpS >= 1) {
+                            parsedReturnSpS = wholeSpS;
+                            returnSpS.innerHTML = parsedReturnSpS;
+                        }
+                };
+                swordPerSeconde();
+                
             }
         });
     });
@@ -120,3 +132,19 @@ function autoclicker () {
 }
 setInterval(autoclicker,1000);
 autoclicker();
+
+function greyitems(){
+    let shopContent = document.querySelectorAll('.shop_content');
+     shopContent.forEach(shopContent => {
+        let itemData = shopContent.dataset.item;
+        let shopItemsCost =  parseFloat(shopItems[itemData].costElement.innerHTML);
+        if (swordNbr < shopItemsCost){
+            shopContent.classList.add('not_enough_money');
+        }
+        else {
+            shopContent.classList.remove('not_enough_money');
+        }
+    });
+}
+greyitems();
+setInterval(greyitems,100);
